835e1990edcb5caffbbc32498d6b1f8a8d98bcc2
brentru <robots199@me.com>
2019-05-28 16:08:24 -0400

    Remove bad digitalio implementation in favor of busdevice implementation of wake

--- adafruit_atecc.py
+++ adafruit_atecc.py
@@ -47,16 +47,19 @@ from adafruit_bus_device.i2c_device import I2CDevice
 __version__ = "0.0.0-auto.0"
 __repo__ = "https://github.com/adafruit/Circuitpython_CircuitPython_CircuitPython_CryptoAuth.git"
 
-_ATECC_ADDR = 0xC0
-_ATECC_DEVICE_ADDR = _ATECC_ADDR >> 1 # i2c address
-_TWLO_TIME = 6e-5 # TWlo, in microseconds
-_WAKE_CLK_FREQ = 100000 # slower clock speed 
-_NORMAL_CLK_FREQ = 1000000 # regular clock speed
+# Device Address
+_REG_ATECC_ADDR = 0xC0
+_REG_ATECC_DEVICE_ADDR = _ATECC_ADDR >> 1
 
-_REVISION = 0x00 # device revision register
-
+# Version Registers
+_REG_REVISION = 0x00 # device version register address
 _ATECC_508_VER = 0x50
 _ATECC_608_VER = 0x60
+
+# Clock constants
+_WAKE_CLK_FREQ = 100000 # slower clock speed 
+_NORMAL_CLK_FREQ = 1000000 # regular clock speed
+_TWLO_TIME = 6e-5 # TWlo, in microseconds
 
 
 class ATECCx08A:
@@ -77,7 +80,6 @@ class ATECCx08A:
         print('device found and awake!')
 
 
-
     def _wake(self):
         """Wakes up THE ATECC608A from sleep or idle modes.
         Returns 1 if device woke up from sleep/idle mode. 
@@ -88,37 +90,51 @@ class ATECCx08A:
         try:
             self.i2c_bus.writeto(_ATECC_ADDR, bytes([b'\x00\x00']), stop=False)
         except:
-            pass # it'll error since the device doesnt exist
+            pass # allow writing to ATECC_ADDR
         # wait for TWLO millis before attempting an i2c scan
         time.sleep(_TWLO_TIME)
-         # check for i2c device
+        # check for an i2c device
         data = self.i2c_bus.scan()
         if data[0] != 96:
             return -1
         return 1
 
+    # pylint: disable=no-member
+    # Reconsider pylint: disable when this can be tested
+    def _read_into(self, address, buf, length=None):
+        # Read a number of bytes from the specified address into the provided
+        # buffer.  If length is not specified (the default) the entire buffer
+        # will be filled.
+        if length is None:
+            length = len(buf)
+        with self._device as device:
+            self._BUFFER[0] = address & 0x7F  # Strip out top bit to set 0
+                                              # value (read).
+            device.write(self._BUFFER, end=1)
+            device.readinto(buf, end=length)
 
-    def _dio_wake(self):
-        """Wakes up THE ATECC608A from sleep or idle modes.
-        Returns 1 if device woke up from sleep/idle mode.
-        """
-        # temporarily deinitialize the i2c bus
-        self.i2c_bus.deinit()
-        import digitalio
-        import board
-        wake_line = digitalio.DigitalInOut(board.SDA)
-        wake_line.direction = digitalio.Direction.OUTPUT
-        wake_line.value = 0
-        time.sleep(_TWLO_TIME)
-        wake_line.value = 1
-        # release the SDA line
-        wake_line.deinit()
-        self.i2c_bus = busio.I2C(board.SCL, board.SDA, frequency = 133000)
-        while not self.i2c_bus.try_lock():
-            pass
-        data = self.i2c_bus.scan()
-        if data[0] != _ATECC_DEVICE_ADDR:
-            return -1 
-        return 1
+    def _read_u8(self, address):
+        # Read a single byte from the provided address and return it.
+        self._read_into(address, self._BUFFER, length=1)
+        return self._BUFFER[0]
 
+    def _write_from(self, address, buf, length=None):
+        # Write a number of bytes to the provided address and taken from the
+        # provided buffer.  If no length is specified (the default) the entire
+        # buffer is written.
+        if length is None:
+            length = len(buf)
+        with self._device as device:
+            self._BUFFER[0] = (address | 0x80) & 0xFF  # Set top bit to 1 to
+                                                       # indicate a write.
+            device.write(self._BUFFER, end=1)
+            device.write(buf, end=length)
 
+    def _write_u8(self, address, val):
+        # Write a byte register to the chip.  Specify the 7-bit address and the
+        # 8-bit value to write to that address.
+        with self._device as device:
+            self._BUFFER[0] = (address | 0x80) & 0xFF  # Set top bit to 1 to
+                                                       # indicate a write.
+            self._BUFFER[1] = val & 0xFF
+            device.write(self._BUFFER, end=2)
