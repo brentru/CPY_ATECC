dbcfddd8cb8d1a2131bfd50bc13a8de6f3226bc9
Ladyada <support@adafruit.com>
2019-08-14 00:33:06 -0400

    can connect/query chip

--- adafruit_atecc.py
+++ adafruit_atecc.py
@@ -86,9 +86,15 @@ class ATECCx08A:
         :param busio i2c_bus: I2C Bus object.
         :param int address: Device address, defaults to _ATECC_DEVICE_ADDR.
         """
+        self._I2CBUF = bytearray(12)
         self._i2c_bus = i2c_bus
         self._i2c_device = None
         self.wakeup()
+        if not self._i2c_device:
+            self._i2c_device = I2CDevice(self._i2c_bus, _REG_ATECC_DEVICE_ADDR, debug=False)
+        self.idle()
+        if (self.version() >> 8) not in (0x50, 0x60):
+            raise RuntimeError("Failed to find 608 or 508 chip")
 
     def wakeup(self):
         """Wakes up THE ATECC608A from sleep or idle modes.
@@ -96,7 +102,11 @@ class ATECCx08A:
         """
         while not self._i2c_bus.try_lock():
             pass
-        #print('i2c bus unlocked!')
+        # check if it exists, first
+        if 0x60 in self._i2c_bus.scan():
+            self._i2c_bus.unlock()
+            return
+
         zero_bits = bytearray(2)
         try:
             self._i2c_bus.writeto(0x0, zero_bits)
@@ -118,12 +128,83 @@ class ATECCx08A:
         if r[0] != 0x11:
             raise RuntimeError("Failed to wakeup")
 
+    def idle(self):
+        self._I2CBUF[0] = 0x2
+        with self._i2c_device as i2c:
+            i2c.write(self._I2CBUF, end=1)
+        time.sleep(0.001)
 
+    def sleep(self):
+        self._I2CBUF[0] = 0x1
+        with self._i2c_device as i2c:
+            i2c.write(self._I2CBUF, end=1)
+        time.sleep(0.001)
 
-    def rev_number(self):
+    def version(self):
         """Returns the ATECC608As revision number
         """
+        self.wakeup()
+        self.idle()
         self._send_command(0x30, 0x00)
+        vers = bytearray(4)
+        self._get_response(vers)
+        return (vers[2] << 8) | vers[3]
+
+    def locked(self):
+        config = bytearray(4)
+        self._read(0, 0x15, config)
+        print([hex(i) for i in config])
+        return config[2] == 0x0 and config[3] == 0x00
+
+    #def random(self, maxrand):
+
+
+    def _read(self, zone, address, buffer):
+        self.wakeup()
+        if len(buffer) not in (4, 32):
+            raise RuntimeError("Only 4 and 32 byte reads supported")
+        if len(buffer) == 32:
+            zone |= 0x80
+        self._send_command(2, zone, address)
+        time.sleep(0.005)
+        self._get_response(buffer)
+        time.sleep(0.001)
+        self.idle()
+
+    def _send_command(self, opcode, param_1, param_2=0x00, data = ''):
+        """Sends a security command packet over i2c.
+        :param byte opcode: The command Opcode
+        :param byte param_1: The first parameter
+        :param byte param_2: The second parameter, can be two bytes.
+        :param byte param_3 data: Optional remaining input data.
+        """
+        # assembling command packet
+        command_packet = bytearray(8+len(data))
+        # word address
+        command_packet[0] = 0x03
+        # i/o group: count
+        command_packet[1] = len(command_packet) - 1 # count
+        # security command packets
+        command_packet[2] = opcode
+        command_packet[3] = param_1
+        command_packet[4] = param_2 & 0xFF
+        command_packet[5] = param_2 >> 8
+        for i,c in enumerate(data):
+            command_packet[6+i] = c
+        print("\tSending:", [hex(i) for i in command_packet])
+        # Checksum, CRC16 verification
+        crc = self.at_crc(command_packet[1:-2])
+        #print('Calculated CRC: ', hex(crc))
+        command_packet[-1] = crc >> 8
+        command_packet[-2] = crc & 0xFF
+        #for i in range(0, len(command_packet)):
+        #    print('command_packet[{0}]: {1}'.format(i, hex(command_packet[i])))
+
+        self.wakeup()
+        with self._i2c_device as i2c:
+            i2c.write(command_packet)
+        # small sleep
+        time.sleep(0.001)
 
 
     def _get_response(self, buf, length=None, retries=20):
@@ -139,64 +220,15 @@ class ATECCx08A:
                     pass
             else:
                 raise RuntimeError("Failed to read data from chip")
-        print([hex(i) for i in response])
+        print("\tReceived: ", [hex(i) for i in response])
         crc = response[-2] | (response[-1] << 8)
-        crc2 = self.at_crc(response[0:2])
+        crc2 = self.at_crc(response[0:-2])
+        #print(hex(crc2))
         if crc != crc2:
             raise RuntimeError("CRC check failure")
         for i in range(length):
             buf[i] = response[i+1]
-
-    def _send_command(self, opcode, param_1, param_2=0x00, data = ''):
-        """Sends a security command packet over i2c.
-        :param byte opcode: The command Opcode
-        :param byte param_1: The first parameter
-        :param byte param_2: The second parameter, can be two bytes.
-        :param byte param_3 data: Optional remaining input data.
-        """
-        # Show Args in REPL
-        print('Opcode: ', opcode)
-        print('Param_1: ', param_1)
-        print('Param_2: ', param_2)
-        print('Data: ', data)
-
-        # assembling command packet
-        command_packet = bytearray(8+len(data))
-        print('Command Size: ', len(command_packet))
-        # word address
-        command_packet[0] = 0x03
-        # i/o group: count
-        command_packet[1] = len(command_packet) - 1 # count
-        # security command packets
-        command_packet[2] = opcode
-        command_packet[3] = param_1
-        command_packet[4] = param_2
-        print(command_packet)
-        for i in range(0, len(command_packet)):
-            print('command_packet[{0}]: {1}'.format(i, command_packet[i]))
-        # Checksum, CRC16 verification
-        crc = self.at_crc(command_packet, len(command_packet) - 2)
-        print('Calculated CRC: ', crc)
-
-
-        # WRITE to ATECC608A
-        with self._device:
-            self._device.write(command_packet)
-        # small sleep
-        time.sleep(2e-6)
-        # RETURNING from ATECC608A
-        # Command completion polling (6.2.2), (6.5)
-        # the size of the group is determined by the command
-        res_size = 3
-        retries = _RX_RETRIES
-        while retries > 0:
-          with self._device:
-            self._device.readinto(self._BUFFER)
-            buff_size = len(self._BUFFER)
-            print(buff_size)
-            retries=-1
-        print(self._BUFFER)
-        print('returned!')
+        return response[1]
 
     def at_crc(self, data, length=None):
         if length is None:
@@ -205,12 +237,17 @@ class ATECCx08A:
             return 0
         polynom = 0x8005
         crc = 0x0
-        for i in range(length):
-            b = data[i]
+        for b in data:
+            #print("\tbyte 0x%02x crc 0x%04x" % (b, crc))
             for shift in range(8):
-                data_bit = int(b & (1<<shift))
+                data_bit = 0
+                if b & (1<<shift):
+                    data_bit = 1
                 crc_bit = (crc >> 15) & 0x1
                 crc <<= 1
+                crc &= 0xFFFF
                 if data_bit != crc_bit:
                     crc ^= polynom
+                    crc &= 0xFFFF
+        #print("\tfinal CRC", hex(crc))
         return crc & 0xFFFF
--- code.py
+++ code.py
@@ -2,10 +2,21 @@
 import board
 import adafruit_atecc
 import busio
+import time
 
 
 _WAKE_CLK_FREQ = 100000 # slower clock speed 	
 i2c = busio.I2C(board.SCL, board.SDA, frequency=_WAKE_CLK_FREQ)
 
 atecc = adafruit_atecc.ATECCx08A(i2c)
-print(atecc.rev_number())
+print("Found ATECC!")
+r = bytearray(1)
+while True:
+    print("Waking up chip")
+    atecc.wakeup()
+    atecc.idle()
+    v = atecc.version()
+    print("Found version 0x%04x" % v)
+    print("Locked?", atecc.locked())
+    time.sleep(1)
+    print("---------------------")
