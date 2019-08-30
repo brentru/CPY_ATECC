bfffb0d4bc9326f264e45a0a45be129e4ae6c66a
brentru <robots199@me.com>
2019-08-30 15:38:22 -0400

    add usage example

--- code.py
+++ code.py
@@ -11,12 +11,19 @@ i2c = busio.I2C(board.SCL, board.SDA, frequency=_WAKE_CLK_FREQ)
 atecc = adafruit_atecc.ATECCx08A(i2c)
 print("Found ATECC!")
 r = bytearray(1)
-while True:
-    print("Waking up chip")
-    atecc.wakeup()
-    atecc.idle()
-    v = atecc.version()
-    print("Found version 0x%04x" % v)
-    print("Locked?", atecc.locked())
-    time.sleep(1)
-    print("---------------------")
+
+print("Waking up chip")
+
+atecc.wakeup()
+
+atecc.idle()
+v = atecc.version()
+
+print("Found version 0x%04x" % v)
+
+
+print("Locked?", atecc.locked())
+
+print("Counter: ", atecc.counter(0))
+
+print(atecc.nonce())
