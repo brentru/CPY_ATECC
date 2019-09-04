ad983db993f3e15acbd92807f96f0a1a63c387e7
brentru <robots199@me.com>
2019-09-04 15:38:56 -0400

    add nonce with example

--- code.py
+++ code.py
@@ -11,5 +11,13 @@ i2c = busio.I2C(board.SCL, board.SDA,
 atecc = adafruit_atecc.ATECCx08A(i2c)
 
 # Generate random number
+print("Generating Random Number...")
 random_num = atecc.random()
 print("Random Number[0]: ", random_num[0])
+
+# Generate a nonce
+print("Generating Nonce...")
+input_data = bytearray(20)
+input_data[3] = 0x03
+nonce_value = atecc.nonce(input_data)
+print("Nonce[0]: ", nonce_value[0])
