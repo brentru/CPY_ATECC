74596d09bb4807ed2ce387c6777372d1dd5da3d5
brentru <robots199@me.com>
2019-09-10 15:37:47 -0400

    add get_version, name, sequence header, issuer, add constants

--- adafruit_atecc/adafruit_atecc_cert_util.py
+++ adafruit_atecc/adafruit_atecc_cert_util.py
@@ -38,6 +38,14 @@ Implementation Notes
 from micropython import const
 import adafruit_atecc.adafruit_atecc_asn1 as asn1
 
+ASN1_INTEGER           = const(0x02)
+ASN1_BIT_STRING        = const(0x03)
+ASN1_NULL              = const(0x05)
+ASN1_OBJECT_IDENTIFIER = const(0x06)
+ASN1_PRINTABLE_STRING  = const(0x13)
+ASN1_SEQUENCE          = const(0x30)
+ASN1_SET               = const(0x31)
+
 class CSR:
     """Certificate signing request generation.
 
@@ -92,13 +100,81 @@ class CSR:
       len_csr_info = self._cert_info._version_len + len_issuer_subject + len_sub_header + len_pub_key + 2
       len_csr_info_header = asn1.seq_header_length(len_csr_info)
 
-      csr_info = bytearray(len_csr_info + len_csr_info_header)
-      csr_out = csr_info
+      print("Version Length:", self._cert_info._version_len)
+      print("len_issuer_subject: ", len_issuer_subject)
+      print("len_sub_header: ", len_sub_header)
+      print("len_pub_key: ", len_pub_key)
+      print("len_csr_info: ", len_csr_info)
+      print("len_csr_info_header: ", len_csr_info_header)
 
       # CSR Info
-      asn1.append_seq_header(len_csr_info, csr_out)
+      #csr_info = bytearray(len_csr_info + len_csr_info_header)
+      csr_info = bytearray()
 
-      # Version
-      asn1.append_version(csr_out)
-      csr_out.append(self._cert_info._version_len)
-      return 1
+      # Append CSR Info
+      self.get_sequence_header(len_csr_info, csr_info)
+      csr_info.append(len_csr_info)
+
+      # Append Version
+      self.get_version(csr_info)
+      csr_info.append(self._cert_info._version_len)
+
+      # Append Subject
+      self.get_sequence_header(len_sub_header, csr_info)
+      csr_info.append(len_sub_header)
+      print(csr_info)
+
+
+    def get_sequence_header(self, length, data):
+        data.append(ASN1_SEQUENCE)
+        if length > 255:
+          data.append(0x82)
+          data.append((length >> 8) & 0xff)
+        elif (length > 127):
+          data.append(0x81)
+
+        data.append((length >> 8) & 0xff)
+
+    def get_version(self, data):
+        """Sets X.509 version"""
+        data[0] = ASN1_INTEGER
+        #  If no extensions are present, but a UniqueIdentifier
+        # is present, the version SHOULD be 2 (value is 1) [4-1-2]
+        data[1] = 0x01
+        data[2] = 0x00
+    
+    def get_name(self, name, type, data):
+        """Appends ASN.1 string in form: set -> seq -> objid -> string
+        :param str name: String to append to buffer.
+        :param int type: Object identifier type.
+        :param bytearray data: Buffer to write to.
+        """
+        data.append(ASN1_SET)
+        data.append(len(name) + 9)
+        
+        data.append(ASN1_SEQUENCE)
+        data.append(len(name) + 7)
+
+        data.append(ASN1_OBJECT_IDENTIFIER)
+        data.append(0x03)
+        data.append(0x55)
+        data.append(0x04)
+        data.append(type)
+
+        data.append(ASN1_PRINTABLE_STRING)
+        data.append(len(name))
+        data.append(name)
+
+
+    def get_issuer_or_subject(self, data):
+      # TODO: relies on impl. of get_name
+      if len(self._country) > 0:
+        data += self.get_name(self._country, 0x06, data)
+      if len(self._state_province) > 0:
+        data += self.get_name(self._state_province, 0x07, data)
+      if len(self._locality) > 0:
+        data += self.get_name(self._locality, 0x0a, data)
+      if len(self._org) > 0:
+        data += self.get_name(self._org, 0x0b, data)
+      if len(self._org_unit) > 0:
+        data += self.get_name(self._org_unit, 0x03, data)
